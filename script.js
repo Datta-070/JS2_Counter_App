41d3e87900b0164c475701b96852ef30a94eb481
Datta-070 <chinmaydatt003@gmail.com>
2023-11-12 11:17:50 +0530

    Code Pushed

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let counter1=0;
 function add1(){
     counter=counter+1;
     document.getElementById('counter').innerText=counter;
+    if(counter=='21' && counter1 !='20'){
+        console.log("Congratulations to Team A 🎉");
+    }
 }
 function add2(){
     counter1=counter1+1;
     document.getElementById('counter1').innerText=counter1;
+    if(counter1==21 && counter !=20){
+        let res1= "Congratulations to Team B 🎉";
+        console.log(res1);
+    }
 }
 function minus1(){
     if(counter>0)
